08746e1d25ad2b28b05d94ad4a78c5af5d906d6d
Nur-beginner <eranew262@gmail.com>
2026-05-05 10:12:11 +0800

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -14,18 +14,12 @@ import vin from "./assets/vinLogo-removebg-preview.webp";
 function App() {
   return (
     <>
-      <main className="min-h-screen bg-[#021827] flex flex-col items-center">
+      <main className="min-h-screen overflow-hidden bg-[#021827] flex flex-col items-center">
         <img
           src={tl}
           alt="TL"
           width="40%"
-          className="fixed animate-tl -top-8 -rotate-20 -left-15"
-        />
-        <img
-          src={spark}
-          alt="Spark"
-          width="40%"
-          className="fixed animate-pulse -top-4 right-17.5"
+          className="absolute animate-tl -top-8 -rotate-20 -left-15"
         />
 
         <header className="bg-[#bd8c1c] animate-bgLogo w-50 h-10 rounded-b-xl flex flex-row-reverse justify-center items-center">
@@ -39,15 +33,29 @@ function App() {
           width="100%"
           className="w-full z-0 fixed top-0 left-0"
         />
-        <h1 className="uppercase text-white font-Girassol text-6xl tracking-wider mt-5">
-          Graduation
-        </h1>
-        <h2 className="uppercase text-white font-Girassol text-2xl tracking-wider">
-          ceremony
-        </h2>
+        <div className="relative w-full ">
+          <h1 className="uppercase text-white text-center font-Girassol text-6xl tracking-wider mt-5">
+            Graduation
+          </h1>
+          <img
+            src={spark}
+            alt="Spark"
+            width="40%"
+            className="absolute animate-pulse -top-4 right-17.5"
+          />
+          <h2 className="uppercase text-white text-center font-Girassol text-2xl tracking-wider">
+            ceremony
+          </h2>
+        </div>
+
         <div className="relative my-5">
           <img src={vin} alt="vin" className="opacity-40" />
-          <img src={hat} alt="hat" width="100%" className="z-1 absolute top-0" />
+          <img
+            src={hat}
+            alt="hat"
+            width="100%"
+            className="z-1 absolute top-0"
+          />
           <img
             src={wave}
             alt="wave"
@@ -61,31 +69,31 @@ function App() {
             className="absolute -top-20"
           />
         </div>
-        <h1 className="font-barberChop transform-[perspective(100px)_rotateX(10deg)_skewX(2deg)] tracking-wider text-[#f4b04b] text-shadow-black text-shadow-lg text-8xl z-2 ml-5">
+        <h1 className="font-barberChop transform-[perspective(100px)_rotateX(10deg)_skewX(2deg)_translateX(5px)] tracking-wider text-[#f4b04b] text-shadow-black text-shadow-lg text-8xl z-2 text-center p-5">
           vintavis
         </h1>
-        <footer className="grid grid-cols-3 border-red-500 border-2 items-center w-full h-fit">
+        <div className="flex flex-col items-center">
+          <p className="text-white text-center uppercase font-LeagueSpartan font-bold text-2xl">
+            12 mei 2026
+          </p>
+          <p className="text-white uppercase font-LeagueSpartan font-bold text-2xl w-75 text-center">
+            man insan cendekia kota kendari
+          </p>
+        </div>
+        <div className="relative w-full">
           <img
             src={bl}
             alt="BL"
-            width="100%"
-            className="border-2 border-red-500 animate-bl -bottom-20 -left-20"
+            width="40%"
+            className="absolute animate-bl -bottom-15 -left-10"
           />
-          <div className="border-2 border-blue-500 flex flex-col items-center">
-            <p className="text-white text-center uppercase font-LeagueSpartan font-bold text-2xl">
-              12 mei 2026
-            </p>
-            <p className="text-white uppercase font-LeagueSpartan font-bold text-2xl w-75 text-center">
-              man insan cendekia kota kendari
-            </p>
-          </div>
           <img
             src={br}
             alt="BR"
-            width="100%"
-            className="border-2 justify-self-end border-red-500 animate-br -bottom-15 -right-20"
+            width="40%"
+            className="absolute animate-br -bottom-15 -right-20"
           />
-        </footer>
+        </div>
       </main>
     </>
   );
