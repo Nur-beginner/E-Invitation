aad9529bac5c89c7fb5dcc96a8dfb33f8611a3ad
Nur-beginner <eranew262@gmail.com>
2026-05-06 10:43:55 +0800

    fixing mark

--- src/components/location.jsx
+++ src/components/location.jsx
@@ -4,11 +4,20 @@ import { useEffect } from "react";
 import L from "leaflet";
 import "leaflet-routing-machine";
 import "leaflet-routing-machine/dist/leaflet-routing-machine.css";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
 function Routing() {
   const map = useMap();
 
   useEffect(() => {
+    const customIcon = new L.Icon({
+      iconUrl: markerIcon,
+      shadowUrl: markerShadow,
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+    });
+
     const control = L.Routing.control({
       waypoints: [
         L.latLng(-4.0548, 122.4816),
@@ -18,9 +27,13 @@ function Routing() {
       ],
       routeWhileDragging: false,
       draggableWaypoints: false,
+
+      createMarker: function (i, wp) {
+        return L.marker(wp.latLng, { icon: customIcon });
+      },
     }).addTo(map);
 
-    return () => map.removeControl(control); // cleanup biar gak numpuk
+    return () => map.removeControl(control);
   }, [map]);
 
   return null;
